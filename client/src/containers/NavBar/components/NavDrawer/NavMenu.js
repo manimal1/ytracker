ebccf3c0aa17048d131c7d78194082277d9dffb3
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2018-12-10 15:41:24 +0100

    Update Navbar to fix menu index updating

--- client/src/containers/NavBar/components/NavDrawer/NavMenu.js
+++ client/src/containers/NavBar/components/NavDrawer/NavMenu.js
@@ -5,26 +5,14 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import BoatIcon from '@material-ui/icons/DirectionsBoat';
-import StoreIcon from '@material-ui/icons/Store';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import RowingIcon from '@material-ui/icons/Rowing';
 
 const NavMenu = (props) => {
-  const { selectedIndex, handleNavMenuItemSelect } = props;
-  const navMenuItems = [
-    { label: 'Dashboard', icon: <DashboardIcon/>, path: '/dashboard' },
-    { label: 'Yachts', icon: <BoatIcon/>, path: '/yachts' },
-    { label: 'Companies', icon: <StoreIcon/>, path: '/company' },
-    { label: 'Captains', icon: <RowingIcon/>, path: '/captains' },
-    { label: 'Users', icon: <AccountCircle/>, path: '/users' },
-  ];
+  const { selectedIndex, navMenu, handleNavMenuItemSelect } = props;
 
   return (
     <List component="nav">
       {
-        navMenuItems.map((item, index) => {
+        navMenu.map((item, index) => {
           return (
             <ListItem
               key={index}
