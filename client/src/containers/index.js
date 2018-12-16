9011e30e6058710b36bf832b458a9077b4555005
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2018-12-16 10:35:03 +0100

    Change routing for crew page from captians page

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -6,5 +6,5 @@ export { default as LoginUser } from './LoginUser';
 export { default as Profile } from './Profile';
 export { default as Yachts } from './Yachts';
 export { default as Company } from './Company';
-export { default as Captains } from './Captains';
+export { default as Crew } from './Crew';
 export { default as Users } from './Users';
