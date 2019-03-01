19b968a76770931343698c5f47dbfc88276b5ee4
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2019-03-02 00:08:05 +0100

    Add new YachtsHome files

---
+++ client/src/containers/Yachts/components/YachtsHome/index.js
@@ -0,0 +1,6 @@
+import { default as YachtsHome } from './YachtsHome';
+// import * as actions from './actions';
+// import reducer from './reducer';
+
+// export { actions, reducer};
+export default YachtsHome;
