08d8d3b1ae8f1dc8166653c8496b612c3b2a02bd
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2019-03-05 21:45:08 +0100

    Update YachtService functionality and object model

--- models/Service.js
+++ models/Service.js
@@ -6,92 +6,95 @@ const { Schema } = mongoose;
 const serviceSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   yacht: {
     type: Schema.Types.ObjectId,
-    ref: 'Yacht',
+    ref: 'Yacht'
   },
   company: {
     type: Schema.Types.ObjectId,
-    ref: 'Company',
+    ref: 'Company'
   },
   createdby: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'User'
   },
   isPaid: {
     type: Boolean,
-    default: false,
+    default: false
   },
   isCompleted: {
     type: Boolean,
-    default: false,
+    default: false
   },
   assignedDate: {
     type: Date,
-    default: Date.now,
+    default: Date.now
+  },
+  serviceType: {
+    type: String
   },
   invoiceNumber: {
-    type: String,
+    type: String
   },
   costCurrency: {
-    type: String,
+    type: String
   },
   cost: {
-    type: Number,
+    type: Number
   },
   isCostTaxAdded: {
     type: Boolean,
-    default: false,
+    default: false
   },
   isCostTaxIncluded: {
     type: Boolean,
-    default: false,
+    default: false
   },
   costTaxSelected: {
     type: Number,
-    default: 0,
+    default: 0
   },
   costTax: {
     type: Number,
-    default: 0.00,
+    default: 0.0
   },
   costTotal: {
     type: Number,
-    default: 0.00,
+    default: 0.0
   },
   chargedCurrency: {
-    type: String,
+    type: String
   },
   charged: {
     type: Number,
-    default: 0.00,
+    default: 0.0
   },
   isChargedTaxAdded: {
     type: Boolean,
-    default: false,
+    default: false
   },
   isChargedTaxIncluded: {
     type: Boolean,
-    default: false,
+    default: false
   },
   chargedTaxSelected: {
     type: Number,
-    default: 0,
+    default: 0
   },
   chargedTaxPercentageOnTop: {
     type: Number,
-    default: 0,
+    default: 0
   },
   chargedTax: {
     type: Number,
-    default: 0.00,
+    default: 0.0
   },
   chargedTotal: {
     type: Number,
-    default: 0.00,
-  },
+    default: 0.0
+  }
 });
 
 module.exports = mongoose.model('Service', serviceSchema);
